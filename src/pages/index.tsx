b769fb1ac39833052b5762f40110a791ae95a484
Julie <julie.habbestad@hotmail.com>
2025-04-26 12:15:29 +0200

    Created page

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <section>
+            <h1> Home </h1>
+            <h2>h</h2>
+        </section>
+        
+    ) 
+    
+}
+
+export default HomePage;
