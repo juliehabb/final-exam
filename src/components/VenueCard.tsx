4d494db4c14ee731659675a9f5320aa6603daf25
Julie <julie.habbestad@hotmail.com>
2025-04-26 15:47:07 +0200

    Finished styling card

--- src/components/VenueCard.tsx
+++ src/components/VenueCard.tsx
@@ -19,29 +19,29 @@ function VenueCard({   image, title, location, rating, price, amenities, href }:
             {/* Image */}
             <img src={image} alt={title} className="w-full h-52 object-cover rounded-t-2xl" />
             {/* Info */}
-            <div>
+            <div className="p-6">
                 {/* Title and Rating */}
-                <div>
+                <div className="flex items-center justify-between mb-1">
                     <h2 className="text-2xl font-bold">{title}</h2>
-                    <div>
-                        <span>{rating}</span>
-                        <span>★</span>
+                    <div className="flex items-center gap-1">
+                        <span className="font-semibold text-lg">{rating}</span>
+                        <span className="text-yellow-400 text-lg">★</span>
                     </div>
                 </div>
                 {/* Location */}
-                <div>{location}</div>
+                <div className="text-gray-500 mb-4">{location}</div>
                 {/* Amenities */}
-                <div>
+                <div className="flex gap-3 mb-5">
                     {amenities.map((amenity) => (
-                    <div key={amenity}>
+                    <div key={amenity} className="flex items-center gap-1 bg-gray-200 text-gray-600 px-2 py-1 rounded-md text-sm">
                         {amenity}
                     </div>
                     ))}
                 </div>
                 {/* Price */}
-                <div>
-                    <span>Per day:</span>
-                    <span>${price}</span>
+                <div className="flex items-end gap-1">
+                    <span className="text-gray-500 text-lg ">Per day:</span>
+                    <span className="text-2xl font-bold">${price}</span>
                 </div>
             </div>
         </a>
