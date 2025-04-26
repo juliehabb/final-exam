f908d73d37f74b1078f76b12be5560fa637fc67d
Julie <julie.habbestad@hotmail.com>
2025-04-26 15:15:03 +0200

    updated card

--- src/components/VenueCard.tsx
+++ src/components/VenueCard.tsx
@@ -3,23 +3,48 @@ import React from "react";
 type VenueCardPromps = {
     image: string;
     title: string;
-    description: string;
+    location: string;
+    rating: number;
+    price: number;
+    amenities: string[];
     href: string;
 }
 
-function VenueCard({ image, title, description, href }: VenueCardPromps) {
+function VenueCard({   image, title, location, rating, price, amenities, href }: VenueCardPromps) {
     return (
         <a 
-        href={href}
+        href={href} 
+        className="block max-w-sm bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-xl transition"
         >
-            <img src={image} alt={title} />
+            {/* Image */}
+            <img src={image} alt={title} className="w-full h-52 object-cover rounded-t-2xl" />
+            {/* Info */}
             <div>
-                <h5>{title}</h5>
-                <p>{description}</p>
+                {/* Title and Rating */}
+                <div>
+                    <h2 className="text-2xl font-bold">{title}</h2>
+                    <div>
+                        <span>{rating}</span>
+                        <span>★</span>
+                    </div>
+                </div>
+                {/* Location */}
+                <div>{location}</div>
+                {/* Amenities */}
+                <div>
+                    {amenities.map((amenity) => (
+                    <div key={amenity}>
+                        {amenity}
+                    </div>
+                    ))}
+                </div>
+                {/* Price */}
+                <div>
+                    <span>Per day:</span>
+                    <span>${price}</span>
+                </div>
             </div>
-
         </a>
-
     );
 }
 
