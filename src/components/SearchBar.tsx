61cc29100a57d7bc4bbab3f51d9d9d222b294987
Julie <julie.habbestad@hotmail.com>
2025-04-27 12:35:18 +0200

    added main elements

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SearchBar = () => {
+    return (
+        <form >
+            <div></div>
+            <input type="text" />
+            <button></button>
+        </form>
+    )
+}
+
+export default SearchBar;
