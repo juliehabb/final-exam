dc1dcb264f2b8dd5c1c0935169eae7a8ebc7928d
Julie <julie.habbestad@hotmail.com>
2025-04-27 12:33:37 +0200

    Added parent main and searchbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,29 @@ import React from "react";
 import venues from "../mock-data/venues"
 import VenueCard from "../components/VenueCard";
 import NavBar from "../components/nav";
+import SearchBar from "../components/SearchBar";
 
 const HomePage = () => {
     return (
-      <section className="flex flex-wrap gap-8 justify-center py-8">
-        {venues.map((venue) => (
-          <VenueCard
-            key={venue.id}
-            image={venue.image}
-            title={venue.title}
-            location={venue.location}
-            rating={venue.rating}
-            price={venue.price}
-            amenities={venue.amenities}
-            href={`/${venue.id}`}
-          />
-        ))}
-      </section>
+      <main>
+        <section>
+          <SearchBar />
+        </section>
+        <section className="flex flex-wrap gap-8 justify-center py-8">
+          {venues.map((venue) => (
+            <VenueCard
+              key={venue.id}
+              image={venue.image}
+              title={venue.title}
+              location={venue.location}
+              rating={venue.rating}
+              price={venue.price}
+              amenities={venue.amenities}
+              href={`/${venue.id}`}
+            />
+          ))}
+        </section>
+      </main>
     );
   };
 
