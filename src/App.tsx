7f44aef148fb2ad3729be287d80682b398901f9d
Julie <julie.habbestad@hotmail.com>
2025-04-27 14:43:50 +0200

    added path

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import HomePage from "./pages/index"
 
 import './App.css';
 import NavBar from './components/nav';
+import VenuePage from './pages/venue';
 
 function App() {
   return (
     <>
       <NavBar/>
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route path='/' element={<HomePage />} /
+        >
+        <Route path='/venue' element={<VenuePage />} /
+        >
       </Routes>
     </>
   );
