20e71564008f1d4712684b31b7b2420cd6d8d99b
Julie <julie.habbestad@hotmail.com>
2025-04-27 14:43:37 +0200

    Changed name

---
+++ src/pages/venue.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PhotoGallery from "../components/PhotoGallery";
+
+const VenuePage = () => {
+    return (
+        <main>
+            <PhotoGallery/>
+
+        </main>
+    )
+
+}
+
+export default VenuePage;
+
+
