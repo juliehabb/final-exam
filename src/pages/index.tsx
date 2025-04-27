bd91c7422f4fd6d53eb3ba767db4d7fc553a2040
Julie <julie.habbestad@hotmail.com>
2025-04-27 12:14:56 +0200

    closed section element

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,20 +5,21 @@ import NavBar from "../components/nav";
 
 const HomePage = () => {
     return (
-        <section className="flex flex-wrap gap-8 justify-center py-8">
-            {venues.map((venue) => (
-                            <VenueCard
-                            key={venue.id}
-                            image={venue.image}
-                            title={venue.title}
-                            location={venue.location}
-                            rating={venue.rating}
-                            price={venue.price}
-                            amenities={venue.amenities}
-                            href={`/${venue.id}`}
-                          />
-    ))}
+      <section className="flex flex-wrap gap-8 justify-center py-8">
+        {venues.map((venue) => (
+          <VenueCard
+            key={venue.id}
+            image={venue.image}
+            title={venue.title}
+            location={venue.location}
+            rating={venue.rating}
+            price={venue.price}
+            amenities={venue.amenities}
+            href={`/${venue.id}`}
+          />
+        ))}
+      </section>
     );
-}
+  };
 
 export default HomePage;
