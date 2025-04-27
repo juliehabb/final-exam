c6efc92c54e77a54de5fccb9a0b757d7351bec7d
Julie <julie.habbestad@hotmail.com>
2025-04-27 15:01:28 +0200

    Added photogallery

---
+++ src/pages/venue.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PhotoGallery from "../components/PhotoGallery";
+
+const VenuePage = () => {
+    return (
+        <main>
+            <section>
+            <PhotoGallery/>
+            </section>
+            
+
+        </main>
+    );
+
+};
+
+export default VenuePage;
+
+
