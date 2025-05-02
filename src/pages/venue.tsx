e9741f9dda4a6bc3814223e93aa44717c92fa24b
Julie <julie.habbestad@hotmail.com>
2025-05-02 12:04:37 +0200

    Added photogallery and temporary mock images

--- src/pages/venue.tsx
+++ src/pages/venue.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import PhotoGallery from "../components/PhotoGallery";
 
 const VenuePage = () => {
+    //Moxk images
+    const images = [
+        "https://placehold.co/600x400?text=hey",
+        "https://placehold.co/600x400?text=2",
+        "https://placehold.co/600x400?text=3",
+        "https://placehold.co/600x400?text=4",
+        "https://placehold.co/600x400?text=5",
+    ];
+
+
     return (
         <main>
             <section>
-            <PhotoGallery/>
+            <PhotoGallery images={images}/>
             </section>
             
 
