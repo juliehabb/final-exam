d1bb3e110796c86eab0a0adac71ae167aeb98aec
Julie <julie.habbestad@hotmail.com>
2025-05-02 19:14:24 +0200

    Login functionality and ui

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,1 +1,40 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import { AuthForm } from "../components/authform";
+
+export default function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    // TODO: call login API with { username, password }
+    console.log("Login:", { username, password });
+  }
+
+  return (
+    <AuthForm
+      title="Log In"
+      submitLabel="Sign In"
+      onSubmit={handleSubmit}
+      bottomLinkText="No account? Register."
+      bottomLinkTo="/register"
+    >
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+        required
+        className="w-full px-4 py-2 border rounded"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        required
+        className="w-full px-4 py-2 border rounded"
+      />
+    </AuthForm>
+  );
+}
