0ca6ac71d3dd94f18ebe0fa202f178e16777f532
Julie <julie.habbestad@hotmail.com>
2025-05-02 16:15:30 +0200

    Added register and profile page

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,18 @@ import HomePage from "./pages/index"
 import './App.css';
 import NavBar from './components/nav';
 import VenuePage from './pages/venue';
+import ProfilePage from './pages/ProfilePage';
+import RegisterPage from './pages/register';
 
 function App() {
   return (
     <>
       <NavBar/>
       <Routes>
-        <Route path='/' element={<HomePage />} /
-        >
-        <Route path='/venue' element={<VenuePage />} /
-        >
+        <Route path='/' element={<HomePage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/venue' element={<VenuePage />} />
+        <Route path='/profile' element={<ProfilePage />} />
       </Routes>
     </>
   );
