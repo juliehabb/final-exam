54f5b5156ce02de6c518a757316af59efa30e556
Julie <julie.habbestad@hotmail.com>
2025-05-05 10:34:07 +0200

    Added base function

---
+++ src/pages/newVenuePage.tsx
@@ -0,0 +1,10 @@
+
+export default function NewVenuePage() {
+    return (
+        <div className="p-6">
+          <h1 className="text-2xl font-bold">Create New Venue</h1>
+        </div>
+      );
+
+    
+}
