b0a85f70923d84c39c6ef57942399b8d1ccb54c0
Julie <julie.habbestad@hotmail.com>
2025-07-10 17:55:03 +0200

    small aestetic edits to profile header

--- src/components/Profile/ProfileHeader.tsx
+++ src/components/Profile/ProfileHeader.tsx
@@ -13,7 +13,7 @@ type ProfileHeaderProps = {
  */
 export function ProfileHeader({ avatarUrl, name, bio, bannerUrl, editButton }: ProfileHeaderProps) {
   return (
-    <div className="bg-gray-100 rounded-xl overflow-hidden shadow-sm">
+    <div className="bg-gray-100 overflow-hidden shadow-sm">
       {/* Banner */}
       <div className="relative">
         <div className="w-full h-40 bg-blue-400 overflow-hidden">
@@ -27,7 +27,7 @@ export function ProfileHeader({ avatarUrl, name, bio, bannerUrl, editButton }: P
         </div>
 
         {/* Avatar */}
-        <div className="absolute -bottom-12 left-6 w-24 h-24 rounded-full border-4 border-white overflow-hidden shadow-md bg-white">
+        <div className="absolute -bottom-12 left-1/2 transform -translate-x-1/2 w-24 h-24 rounded-full border-4 border-white overflow-hidden shadow-md bg-white">
           <img src={avatarUrl} alt={`${name}'s avatar`} className="w-full h-full object-cover" />
         </div>
 
@@ -36,7 +36,7 @@ export function ProfileHeader({ avatarUrl, name, bio, bannerUrl, editButton }: P
       </div>
 
       {/* Name & Bio */}
-      <div className="pt-16 pb-6 px-6">
+      <div className="pt-16 pb-6 px-6 text-center">
         <h2 className="text-xl font-semibold">{name}</h2>
         <p className="text-gray-600 text-sm mt-1">{bio}</p>
       </div>
